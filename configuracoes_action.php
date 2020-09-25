1d6b3c3019529acbab09b4135a1cbd4bdb8131bb
Cristian Visentini <cristianvisentini@hotmail.com>
2020-09-25 20:14:15 -0300

    Todos os itens de configuracoes podem ser atualizados, incluindo imagens -- sistema OK

--- configuracoes_action.php
+++ configuracoes_action.php
@@ -40,11 +40,11 @@ if ($Name && $Email) {
 
     if (count($Birthdate) != 3) {
         $_SESSION['flash'] = 'Data de Nascimento inválida';
-        header('Location: '.$Base.'/configuracoes.php');
+        header('Location: ' . $Base . '/configuracoes.php');
         exit;
     }
 
-    $Birthdate = $Birthdate[2].'-'.$Birthdate[1].'-'.$Birthdate[0];
+    $Birthdate = $Birthdate[2] . '-' . $Birthdate[1] . '-' . $Birthdate[0];
 
     if (strtotime($Birthdate) === false) {
         $_SESSION['flash'] = 'Data de Nascimento inválida';
@@ -67,6 +67,115 @@ if ($Name && $Email) {
         }
     }
 
+    //Avatar
+
+    if (isset($_FILES['avatar']) && !empty($_FILES['avatar']['tmp_name'])) {
+        $NewAvatar = $_FILES['avatar'];
+        if (in_array($NewAvatar['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
+            $AvatarWidth = 200;
+            $CoverHeight = 200;
+
+
+            list($WidthOrig, $HeighOrig) = getimagesize($NewAvatar['tmp_name']);
+            $Ratio = $WidthOrig / $HeighOrig;
+
+            $NewWidth = $AvatarWidth;
+            $NewHeight = $NewWidth / $Ratio;
+
+            if ($NewHeight < $CoverHeight) {
+                $NewHeight = $CoverHeight;
+                $NewWidth = $CoverHeight * $Ratio;
+            }
+
+
+
+            $X = $AvatarWidth - $NewWidth;
+            $Y = $CoverHeight - $NewHeight;
+            $X = $X < 0 ? $X / 2 : $X;
+            $Y = $Y < 0 ? $Y / 2 : $Y;
+
+            $FinalImage = imagecreatetruecolor($AvatarWidth, $CoverHeight);
+
+            switch ($NewAvatar['type']) {
+                case 'image/jpeg':
+                case 'image/jpg':
+                    $Image = imagecreatefromjpeg($NewAvatar['tmp_name']);
+                    break;
+                case 'image/png':
+                    $Image = imagecreatefrompng($NewAvatar['tmp_name']);
+                    break;
+            }
+
+            imagecopyresampled(
+                $FinalImage, $Image,
+                $X, $Y, 0, 0,
+                $NewWidth, $NewHeight,
+                $WidthOrig, $HeighOrig
+            );
+
+            $AvatarName = md5(time().rand(0, 999).'.jpg');
+
+            imagejpeg($FinalImage, './media/avatars/'.$AvatarName, 100);
+
+            $UserInfo->Avatar = $AvatarName;
+
+        }
+    }
+
+     //Cover
+    
+     if (isset($_FILES['cover']) && !empty($_FILES['cover']['tmp_name'])) {
+        $NewCover= $_FILES['cover'];
+        if (in_array($NewCover['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
+            $CoverWidth = 850;
+            $CoverHeight = 313;
+
+
+            list($WidthOrig, $HeighOrig) = getimagesize($NewCover['tmp_name']);
+            $Ratio = $WidthOrig / $HeighOrig;
+
+            $NewWidth = $CoverWidth;
+            $NewHeight = $NewWidth / $Ratio;
+
+            if ($NewHeight < $CoverHeight) {
+                $NewHeight = $CoverHeight;
+                $NewWidth = $CoverHeight * $Ratio;
+            }
+
+
+
+            $X = $CoverWidth - $NewWidth;
+            $Y = $CoverHeight - $NewHeight;
+            $X = $X < 0 ? $X / 2 : $X;
+            $Y = $Y < 0 ? $Y / 2 : $Y;
+
+            $FinalImage = imagecreatetruecolor($CoverWidth, $CoverHeight);
+
+            switch ($NewCover['type']) {
+                case 'image/jpeg':
+                case 'image/jpg':
+                    $Image = imagecreatefromjpeg($NewCover['tmp_name']);
+                    break;
+                case 'image/png':
+                    $Image = imagecreatefrompng($NewCover['tmp_name']);
+                    break;
+            }
+
+            imagecopyresampled(
+                $FinalImage, $Image,
+                $X, $Y, 0, 0,
+                $NewWidth, $NewHeight,
+                $WidthOrig, $HeighOrig
+            );
+
+            $CoverName = md5(time().rand(0, 999).'.jpg');
+
+            imagejpeg($FinalImage, './media/covers/'.$CoverName, 100);
+
+            $UserInfo->Cover = $CoverName;
+
+        }
+    }
 
 
     $UserDao->Update($UserInfo);
